f15841a2880236856c2a67eefb5d66db45681167
janzitniak <jan.zitniak@gmail.com>
2023-04-24 16:31:52 +0200

    Doplnenie rôznych scenárov ako prerušiť cyklus. Ukážka na continue, break, System.exit.

--- src/Continue.java
+++ src/Continue.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Continue {
     public static void main(String[] args) {
 
@@ -18,4 +20,6 @@ public class Continue {
         System.out.println("Koniec programu");
 
     }
+
+
 }
